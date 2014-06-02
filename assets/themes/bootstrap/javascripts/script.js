5b1f112c773dc8f377bd5078ceec6f79f2064a4b
Matt Petrie <matthew.petrie@gmail.com>
2014-06-02 02:19:49 -0700

    added smooth scrolling effect

--- assets/themes/bootstrap/javascripts/script.js
+++ assets/themes/bootstrap/javascripts/script.js
@@ -1,8 +1,21 @@
-$(
+$(function(){
   $('#contact-button').click(function(event){
     event.preventDefault();
     $('#email-button').fadeToggle(400);
     $('#github-button').fadeToggle(600);
     $('#twitter-button').fadeToggle(800);
-  })
-);
+  });
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
