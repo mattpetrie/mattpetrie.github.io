8735247a89546798cacaa380a69e043326ac1777
Matt Petrie <matthew.petrie@gmail.com>
2014-06-02 23:01:12 -0700

    reworked profile statement and redesigned navbar

--- assets/themes/bootstrap/javascripts/script.js
+++ assets/themes/bootstrap/javascripts/script.js
@@ -6,6 +6,16 @@ $(function(){
     $('#twitter-button').fadeToggle(800);
   });
 
+  var $win = $(window);
+  var $li = $('.nav li').first();
+  $win.scroll(function(){
+    if($win.scrollTop() > 100){
+      $li.fadeIn();
+    } else {
+      $li.fadeOut();
+    }
+  });
+
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
