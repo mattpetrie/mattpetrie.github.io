b0ccbdd02a7c3614db9618db83ab0c8da8c9e493
Matt Petrie <matthew.petrie@gmail.com>
2014-06-11 01:34:47 -0700

    basic minimalist design with rotate effect working

--- assets/themes/bootstrap/javascripts/script.js
+++ assets/themes/bootstrap/javascripts/script.js
@@ -1,31 +1,48 @@
+// $(function(){
+//   $('#contact-button').click(function(event){
+//     event.preventDefault();
+//     $('#email-button').fadeToggle(400);
+//     $('#github-button').fadeToggle(600);
+//     $('#twitter-button').fadeToggle(800);
+//   });
+
+//   var $win = $(window);
+//   var $li = $('.nav li').first();
+//   $win.scroll(function(){
+//     if($win.scrollTop() > 100){
+//       $li.fadeIn();
+//     } else {
+//       $li.fadeOut();
+//     }
+//   });
+
+//   $('a[href*=#]:not([href=#])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000);
+//         return false;
+//       }
+//     }
+//   });
+// });
 $(function(){
-  $('#contact-button').click(function(event){
-    event.preventDefault();
-    $('#email-button').fadeToggle(400);
-    $('#github-button').fadeToggle(600);
-    $('#twitter-button').fadeToggle(800);
-  });
+  setInterval(toggleFlip, 5000);
 
-  var $win = $(window);
-  var $li = $('.nav li').first();
-  $win.scroll(function(){
-    if($win.scrollTop() > 100){
-      $li.fadeIn();
+  var flipped = false
+  var words = ["Ruby on Rails", "Backbone.js", "Bootstrap", "HTML5", "CSS3", "RSpec", "Capybara", "JavaScript", "Sass", "Node.js"]
+  function toggleFlip(){
+    $('.rotator-inner').toggleClass('flip');
+    var text = words[Math.floor(Math.random() * words.length)];
+    if(flipped === false){
+      $('.back p').html(text);
+      flipped = true;
     } else {
-      $li.fadeOut();
+      $('.front p').html(text);
+      flipped = false;
     }
-  });
-
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+  }
+})
