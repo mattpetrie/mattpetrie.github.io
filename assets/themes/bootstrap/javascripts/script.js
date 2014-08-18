35d7f4d89c917216574c659c7fb08a730c224d26
Matt Petrie <matthew.petrie@gmail.com>
2014-08-18 11:24:14 -0700

    modified word selectors to cycle rather than choose randomly

--- assets/themes/bootstrap/javascripts/script.js
+++ assets/themes/bootstrap/javascripts/script.js
@@ -11,34 +11,43 @@ $(function(){
     } else {
       showIntro();
     }
-  }
+  };
   setInterval(swapTech, 6000);
   setTimeout(function(){
-    setInterval(swapAdjective, 6000)
+    setInterval(swapAdjective, 6000);
   }, 3000);
 
-  var flipped = false
-  var words1 = ["Ruby on Rails", "JavaScript", "jQuery", "SQL", "HTML5", "CSS3"] 
-  var words2 = ["Backbone.js", "RSpec", "Capybara", "Sass", "Node.js", "CoffeeScript", "Git"]
+  var flipped = false;
+  var words1 = ["Ruby on Rails", "JavaScript", "jQuery", "SQL", "HTML5", "CSS3", "Grunt.js"]; 
+  var words2 = ["Backbone.js", "RSpec", "Capybara", "Sass", "Node.js", "Mocha", "CoffeeScript", "Git"];
+  var words1Counter = 0;
+  var words2Counter = 0;
+
   function swapTech(){
+    var text;
     $('.rotator-inner').toggleClass('flipX');
     if(flipped === false){
-       var text = words2[Math.floor(Math.random() * words2.length)];
+       text = words2[words2Counter];
+       words2Counter = (words2Counter === words2.length) ? 0 : words2Counter + 1;
       $('.back p').html(text);
       flipped = true;
     } else {
-      var text = words1[Math.floor(Math.random() * words1.length)];
+      text = words1[words1Counter];
+      words1Counter = (words1Counter === words1.length) ? 0 : words1Counter + 1;
       $('.front p').html(text);
       flipped = false;
     }
   }
 
-  var adjFlipped = false
+  var adjFlipped = false;
   words3 = ["dynamic", "useful", "impactful", "efficient", "responsive",
-    "effective", "innovative", "adaptable", "spectacular", "interactive", "beautiful"]
+    "effective", "innovative", "adaptable", "spectacular", "interactive", "beautiful"];
+  var words3Counter = 0;
+
   function swapAdjective(){
     $("#adjective-inner").toggleClass('flipY');
-    var text = words3[Math.floor(Math.random() * words3.length)];
+    var text = words3[words3Counter];
+    words3Counter = (words3Counter === words3.length) ? 0 : words3Counter + 1;
     if(adjFlipped === false){
       $('#adj-back p').html(text);
       adjFlipped = true;
@@ -65,4 +74,4 @@ $(function(){
     $('#portfolio').fadeOut();
     $('#intro').delay(600).fadeIn('slow');
   }
-})
+});
